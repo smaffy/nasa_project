ab8ecd2b279efe9be4e1f9fddb486732c17ec434
smafy <rudakovacz@gmail.com>
2020-06-09 20:07:19 +0200

    banners in admin

--- texts/admin.py
+++ texts/admin.py
@@ -5,7 +5,7 @@ from django import forms
 from parler.admin import TranslatableAdmin
 
 from .forms import PageTextsAdminForm
-from .models import PageTexts
+from .models import PageTexts, PagePictures
 
 
 @admin.register(PageTexts)
@@ -14,19 +14,32 @@ class PageTextsAdmin(TranslatableAdmin):
     list_display = ('name', 'active')
     form = PageTextsAdminForm
 
-    # if form.cleaned_data.get('name') == 'footer_texts':
-    #     form.fields['image'].widget = forms.HiddenInput()
-
     class Meta:
         proxy = True
 
-    # def has_delete_permission(self, request, obj=None):
-    #     # Disable delete
-    #     return False
+    def has_delete_permission(self, request, obj=None):
+        # Disable delete
+        return False
 
     def has_add_permission(self, request):
         # Disable add
         return False
 
+
+@admin.register(PagePictures)
+class PagePicturesAdmin(TranslatableAdmin):
+    save_on_top = True
+    list_display = ('name', 'active')
+
+    class Meta:
+        proxy = True
+
+    def has_delete_permission(self, request, obj=None):
+        # Disable delete
+        return False
+
+    def has_add_permission(self, request):
+        # Disable add
+        return False
 
 
